39daf1cc5c428d38616cd324105cfb0c6182ada5
aliasav <saurabhav24@gmail.com>
2020-07-26 15:52:50 +0530

    add migrations

--- urlshortener/web/admin.py
+++ urlshortener/web/admin.py
@@ -1,3 +1,14 @@
+"""Django admin config for web app
+"""
 from django.contrib import admin
+from web.models import URL
 
-# Register your models here.
+class URLAdmin(admin.ModelAdmin):
+    """
+    Modeladmin for URL
+    """
+    list_display = ("id", "short_url", "original_url", "status")
+    search_fields = ["short_url", "original_url"]
+    readonly_fields = ['created_at',]
+
+admin.site.register(URL, URLAdmin)
--- urlshortener/web/models.py
+++ urlshortener/web/models.py
@@ -1,3 +1,40 @@
 from django.db import models
+from django.contrib.postgres.fields import JSONField
 
-# Create your models here.
+
+class URL(models.Model):
+    '''Model for short-long URL mapping
+    '''
+    # original absolute URL
+    original_url = models.CharField(max_length=1024, null=True, blank=False)
+
+    # hash value
+    hash_value = models.CharField(max_length=20, null=True, blank=False)
+
+    # shortened absolute URL
+    short_url = models.CharField(max_length=512, null=True, blank=False)
+
+    # status of the original URL website
+    INIT = 0
+    LIVE = 1
+    BLOCKED = 2
+    EXPIRED = 3
+    status_choices = [
+        (INIT, 'init'),
+        (LIVE, 'live'),
+        (BLOCKED, 'blocked'),
+        (EXPIRED, 'expired'),
+    ]
+    status = models.IntegerField(default=0, choices=status_choices)
+
+    # expiry datetime of the short URL
+    expiry = models.DateTimeField(null=True, blank=True)
+
+    # created at timestamp
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    # meta data: hashing algorithm, origial url headers
+    meta = JSONField(default=dict, null=True, blank=True)
+
+    def __str__(self):
+        return f'{self.short_url}<-->{self.original_url}'
